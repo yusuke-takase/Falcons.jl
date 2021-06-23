fc71951d03687152ed1c9a28d2325e6e970069eb
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2021-06-23 18:20:17 +0900

    new function added

--- src/.ipynb_checkpoints/Falcons-checkpoint.jl
+++ src/.ipynb_checkpoints/Falcons-checkpoint.jl
@@ -12,6 +12,7 @@ include("./function/scanning.jl")
 include("./function/mapmake.jl")
 
 export ScanningStrategy, gen_ScanningStrategy, get_pointings, get_pointing_pixels, period2rpm
+export TwoTelescopes_ScanningStrategy2map, ThreeTelescopes_ScanningStrategy2map
 export ScanningStrategy2map, Mapmaking, pixtod2hitmap, angtod2hitmap, Genmap
 
 end
--- src/function/.ipynb_checkpoints/mapmake-checkpoint.jl
+++ src/function/.ipynb_checkpoints/mapmake-checkpoint.jl
@@ -113,6 +113,105 @@ function ScanningStrategy2map(SS::ScanningStrategy, division::Int)
     return out_map
 end
 
+function TwoTelescopes_ScanningStrategy2map(SS1::ScanningStrategy, SS2::ScanningStrategy, division::Int)
+    resol = Resolution(SS1.nside)
+    npix = nside2npix(SS1.nside)
+    
+    month = Int(SS1.duration / division)
+    ω_hwp = rpm2angfreq(SS1.hwp_rpm)
+    
+    hit_map = zeros(npix)
+    Cross = zeros((2,4, npix))
+    BEGIN = 0
+    p = Progress(division)
+    @views @inbounds for i = 1:division
+        END = i * month
+        pix_tod1, psi_tod1, time_array1 = get_pointing_pixels(SS1, BEGIN, END)
+        pix_tod2, psi_tod2, time_array2 = get_pointing_pixels(SS2, BEGIN, END)
+        pix_tod = @views [pix_tod1 pix_tod2]
+        psi_tod = @views [psi_tod1 psi_tod2]
+        @views @inbounds for j = eachindex(psi_tod[1,:])
+            pix_tod_jth_det = pix_tod[:,j]
+            #psi_tod_jth_det = psi_tod[:,j]
+            psi_tod_jth_det = ifelse(ω_hwp == 0.0, -psi_tod[:,j], psi_tod[:,j])
+            @views @inbounds @simd for k = eachindex(psi_tod[:,1])
+                t = time_array1[k]
+                ipix = pix_tod_jth_det[k]
+                psi = psi_tod_jth_det[k]
+                hwp_ang = 4ω_hwp*t
+                
+                hit_map[ipix] += 1
+                Cross[1,1,ipix] += sin(hwp_ang - psi)
+                Cross[2,1,ipix] += cos(hwp_ang - psi)
+                Cross[1,2,ipix] += sin(hwp_ang - 2psi)
+                Cross[2,2,ipix] += cos(hwp_ang - 2psi)
+                Cross[1,3,ipix] += sin(hwp_ang - 3psi)
+                Cross[2,3,ipix] += cos(hwp_ang - 3psi)
+                Cross[1,4,ipix] += sin(hwp_ang - 4psi)
+                Cross[2,4,ipix] += cos(hwp_ang - 4psi)
+            end
+        end
+        BEGIN = END
+        next!(p)
+    end
+    link1 = @views @. (Cross[1,1,:]/hit_map)^2 + (Cross[2,1,:]/hit_map)^2
+    link2 = @views @. (Cross[1,2,:]/hit_map)^2 + (Cross[2,2,:]/hit_map)^2
+    link3 = @views @. (Cross[1,3,:]/hit_map)^2 + (Cross[2,3,:]/hit_map)^2
+    link4 = @views @. (Cross[1,4,:]/hit_map)^2 + (Cross[2,4,:]/hit_map)^2
+    out_map = @views [hit_map, link1, link2, link3, link4]
+    return out_map
+end
+
+function ThreeTelescopes_ScanningStrategy2map(SS1::ScanningStrategy, SS2::ScanningStrategy, SS3::ScanningStrategy, division::Int)
+    resol = Resolution(SS1.nside)
+    npix = nside2npix(SS1.nside)
+    
+    month = Int(SS1.duration / division)
+    ω_hwp = rpm2angfreq(SS1.hwp_rpm)
+    
+    hit_map = zeros(npix)
+    Cross = zeros((2,4, npix))
+    BEGIN = 0
+    p = Progress(division)
+    @views @inbounds for i = 1:division
+        END = i * month
+        pix_tod1, psi_tod1, time_array1 = get_pointing_pixels(SS1, BEGIN, END)
+        pix_tod2, psi_tod2, time_array2 = get_pointing_pixels(SS2, BEGIN, END)
+        pix_tod3, psi_tod3, time_array3 = get_pointing_pixels(SS3, BEGIN, END)
+        pix_tod = @views [pix_tod1 pix_tod2 pix_tod3]
+        psi_tod = @views [psi_tod1 psi_tod2 psi_tod3]
+        @views @inbounds for j = eachindex(psi_tod[1,:])
+            pix_tod_jth_det = pix_tod[:,j]
+            #psi_tod_jth_det = psi_tod[:,j]
+            psi_tod_jth_det = ifelse(ω_hwp == 0.0, -psi_tod[:,j], psi_tod[:,j])
+            @views @inbounds @simd for k = eachindex(psi_tod[:,1])
+                t = time_array1[k]
+                ipix = pix_tod_jth_det[k]
+                psi = psi_tod_jth_det[k]
+                hwp_ang = 4ω_hwp*t
+                
+                hit_map[ipix] += 1
+                Cross[1,1,ipix] += sin(hwp_ang - psi)
+                Cross[2,1,ipix] += cos(hwp_ang - psi)
+                Cross[1,2,ipix] += sin(hwp_ang - 2psi)
+                Cross[2,2,ipix] += cos(hwp_ang - 2psi)
+                Cross[1,3,ipix] += sin(hwp_ang - 3psi)
+                Cross[2,3,ipix] += cos(hwp_ang - 3psi)
+                Cross[1,4,ipix] += sin(hwp_ang - 4psi)
+                Cross[2,4,ipix] += cos(hwp_ang - 4psi)
+            end
+        end
+        BEGIN = END
+        next!(p)
+    end
+    link1 = @views @. (Cross[1,1,:]/hit_map)^2 + (Cross[2,1,:]/hit_map)^2
+    link2 = @views @. (Cross[1,2,:]/hit_map)^2 + (Cross[2,2,:]/hit_map)^2
+    link3 = @views @. (Cross[1,3,:]/hit_map)^2 + (Cross[2,3,:]/hit_map)^2
+    link4 = @views @. (Cross[1,4,:]/hit_map)^2 + (Cross[2,4,:]/hit_map)^2
+    out_map = @views [hit_map, link1, link2, link3, link4]
+    return out_map
+end
+
 function Genmap(map_array::Array)
     nside = npix2nside(length(map_array))
     m = Map{Float64, RingOrder}(nside)
